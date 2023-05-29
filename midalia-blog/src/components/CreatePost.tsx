ddb8696f3111803558a40ae01cd5433a81eb0b38
Emad Dehnavi <dehnavi@gmx.com>
2023-05-29 23:20:11 +0200

    Adding text editor

--- midalia-blog/src/components/CreatePost.tsx
+++ midalia-blog/src/components/CreatePost.tsx
@@ -1,20 +1,23 @@
-import {
-  FaBold,
-  FaItalic,
-  FaUnderline,
-  FaStrikethrough,
-  FaLink,
-  FaAlignCenter,
-  FaAlignJustify,
-  FaAlignLeft,
-  FaAlignRight,
-} from "react-icons/fa";
-import {
-  MdOutlineAddPhotoAlternate,
-  MdOutlineEmojiEmotions,
-} from "react-icons/md";
+import { Editor, EditorState } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import { useState } from "react";
 
 export const CreatePost: React.FC = () => {
+  const [postEntry, setPostEntry] = useState<EditorState>();
+
+  const handleTextEditorChange = (e: EditorState) => {
+    setPostEntry(e);
+  };
+
+  const handleNewPost = () => {
+    if (postEntry) {
+      // TODO: See how we can store with applied style & font formatting
+      // const plainObject = postEntry.toJS();
+    }
+    return;
+  };
+
   return (
     <>
       <div className="turncate m-8 flex h-fit flex-1 flex-col rounded bg-stone-100 pb-4 text-black sm:flex-initial">
@@ -22,73 +25,17 @@ export const CreatePost: React.FC = () => {
           <h2>Create a new post</h2>
         </div>
         <div className="ml-4 mr-4 flex flex-col justify-center gap-4 bg-transparent text-center font-bold text-slate-700">
-          <div className="flex flex-row gap-2">
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaBold className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaItalic className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaUnderline className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaStrikethrough className="text-sm" />
-              </div>
-            </button>
-            <div className="h-6 w-1 flex-initial border-l-2 border-slate-300 bg-transparent"></div>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaAlignJustify className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaAlignLeft className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaAlignCenter className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaAlignRight className="text-sm" />
-              </div>
-            </button>
-            <div className="h-6 w-1 flex-initial border-l-2 border-slate-300 bg-transparent"></div>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <FaLink className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <MdOutlineAddPhotoAlternate className="text-sm" />
-              </div>
-            </button>
-            <button className="h-6 w-6 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
-              <div className="flex flex-1 justify-center">
-                <MdOutlineEmojiEmotions className="text-sm" />
-              </div>
-            </button>
-          </div>
           <input
             className="border-1 w-full rounded border-neutral-900 p-2"
             type="text"
             placeholder="Post title"
           />
-          <textarea
-            className="border-1 h-64 w-full rounded border-neutral-900 p-2"
-            placeholder="Write whatever is in your mind..."
+          <Editor
+            editorState={postEntry}
+            toolbarClassName="toolbarClassName bg-white flex"
+            wrapperClassName="wrapperClassName bg-white flex flex-col h-96"
+            editorClassName="editorClassName bg-white"
+            onEditorStateChange={handleTextEditorChange}
           />
           <div className="flex flex-row-reverse ">
             <input type="checkbox" className="flex-initial" />{" "}
@@ -97,7 +44,10 @@ export const CreatePost: React.FC = () => {
             </div>
           </div>
           <div className="flex flex-row-reverse ">
-            <button className="h-8 w-24 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
+            <button
+              onClick={handleNewPost}
+              className="h-8 w-24 flex-initial items-center justify-center border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent"
+            >
               <div className="flex flex-1 justify-center">Post</div>
             </button>
           </div>
