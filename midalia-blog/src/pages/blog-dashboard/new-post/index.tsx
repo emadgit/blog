331315cc606916024cfbb1c02decdda52ffce347
Emad Dehnavi <dehnavi@gmx.com>
2023-06-11 22:43:13 +0200

    More style changes

--- midalia-blog/src/pages/blog-dashboard/new-post/index.tsx
+++ midalia-blog/src/pages/blog-dashboard/new-post/index.tsx
@@ -31,7 +31,7 @@ const NewPost: NextPage = () => {
       <main className="h-screen w-screen overflow-auto bg-gradient-to-b from-[#ffffff] to-[#ffffff]">
         <div className="flex h-full flex-col-reverse bg-gray-300 sm:h-full sm:flex-row">
           <BlogNavbar />
-          <div className="flex flex-1 flex-col bg-white h-fit sm:h-full">
+          <div className="flex flex-1 flex-col bg-white h-fit sm:h-full overflow-auto w-full">
            <CreatePost />
           </div>
         </div>
