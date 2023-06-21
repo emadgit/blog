7cdafde31e00abc2ff1d900463380d73c5e41550
Emad Dehnavi <dehnavi@gmx.com>
2023-06-21 19:02:31 +0200

    edit profile - ui

--- midalia-blog/src/components/ProfileEditableForm.tsx
+++ midalia-blog/src/components/ProfileEditableForm.tsx
@@ -7,6 +7,35 @@ export const ProfileEditableForm: React.FC = () => {
           <div className="p-4 text-2xl">
             <h2>Profile</h2>
           </div>
+          <div className="flex flex-col sm:flex-row justify-center items-center">
+            <div className="flex w-full p-4">
+              <input className="w-full p-4" placeholder="Add your skills, seperete them with (,) comma."/>
+            </div>
+            <div className="flex w-full p-4">
+              <input className="w-full p-4" placeholder="Your Linkedin Profile" />
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row justify-center items-center">
+            <div className="flex w-full p-4">
+              <input className="w-full p-4" placeholder="Your Stackoverflow profile"/>
+            </div>
+            <div className="flex w-full p-4">
+              <input className="w-full p-4" placeholder="Your Medium profile" />
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row justify-center items-center">
+            <div className="flex w-full p-4">
+              <textarea className="w-full h-64 p-2" placeholder="Tell us about yourself, this will be show in your homepage as a bio."/>
+            </div>
+            <div className="flex w-full p-4">
+              <textarea className="w-full h-64 p-3" placeholder="Write a message to engage your audience and invite them to connect. This will be show before Socials section." />
+            </div>
+          </div>
+          <div className="flex w-full justify-end p-4">
+          <button className="h-8 w-24 flex-initial border-2 border-slate-600 bg-zinc-200 shadow-md hover:bg-transparent">
+              <div className="flex flex-1 justify-center">Save</div>
+            </button>
+          </div>
         </div>
       </>
     );
